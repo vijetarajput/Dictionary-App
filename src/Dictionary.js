ff3f0a347825afefda7e5840940357ef314e7227
Vijeta Singh Rajput <vijetasinghrajput@gmail.com>
2023-12-05 17:53:29 +0000

    Dictionary Api integration added.

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,26 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
+
 export default function Dictionary()
 {
     let [keyword, setKeyword]=useState("");
+
     function handelKeywordChange(event){
         setKeyword(event.target.value);
     }
+
+    function handelResponse(response){
+        console.log(response.data[0].meanings[0].definitions[0]);
+        let value = response.data[0].meanings[0].definitions[0];
+        alert(`Searching for ${value}`);
+    }
+
     function onSearchEvent(event)
     {
-        alert(`Searching for ${keyword}`);
+        event.preventDefault();
+        let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+        axios.get(apiUrl).then(handelResponse);
     }
 return(
     <div className="Dictionary">
