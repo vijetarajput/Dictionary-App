6a73b607e225fe37fc27016b0783f251a0abe458
Vijeta Singh Rajput <vijetasinghrajput@gmail.com>
2023-12-07 15:07:50 +0000

    Styling updated.

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -13,13 +13,20 @@ export default function Dictionary()
         setKeyword(event.target.value);
     }
 
-    function handelResponse(response){
+    function handelDictionaryResponse(response){
         console.log(response.data[0]);
         setResult(response.data[0]);
     }
+    function handelPexelsResponse(response){
+        console.log(response);
+    }
     function search(){
         let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-        axios.get(apiUrl).then(handelResponse);
+        axios.get(apiUrl).then(handelDictionaryResponse);
+
+        let pexelsApiKey = "GDYIxCacYzLBQ3YFU7illkNHQa5ufZkcrU1jOopX3BQTCga6ALOJTLP9";
+        let pexelsApiUrl =`https://api.pexels.com/v1/search?query=${keyword}`;
+        axios.get(pexelsApiUrl,{headers:{"Authorisation":`Bearer ${pexelsApiKey}`}}).then(handelPexelsResponse);
     }
     function onSubmitEvent(event)
     {
@@ -39,12 +46,13 @@ export default function Dictionary()
         
         <section >
             <form className="form" onSubmit={onSubmitEvent}>
-                <div className="search">
+                <h4>What word do ypu want to look up?</h4>
                 <input type="search" defaultValue={keyword} autoFocus={true} onChange={handelKeywordChange}/>
                 <button type="submit" className="button">Submit</button>
-                </div>
+                <div className="hint">Suggested words: sunset, wind, yoga, forest...</div>
+                
                 </form>
-            <div className="hint">Suggested words: sunset, wind, yoga, forest...</div>
+            
         </section>
         <Results result={result}/>
             
